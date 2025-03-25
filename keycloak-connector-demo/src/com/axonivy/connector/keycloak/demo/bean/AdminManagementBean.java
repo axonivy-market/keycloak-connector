9dd737ffc83ba03aa483edf7360a00a7ad48e83a
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-03-25 18:03:14 +0700

    update code

--- keycloak-connector-demo/src/com/axonivy/connector/keycloak/demo/bean/AdminManagementBean.java
+++ keycloak-connector-demo/src/com/axonivy/connector/keycloak/demo/bean/AdminManagementBean.java
@@ -1,166 +1,53 @@
 package com.axonivy.connector.keycloak.demo.bean;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-
 import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 import org.primefaces.model.file.UploadedFile;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.axonivy.connector.keycloak.demo.utils.JarUtils;
 
 @ViewScoped
 @ManagedBean
 public class AdminManagementBean {
+  private static final String TEMP_FILE_NAME = "keycloakThemes";
+  private static final String JAR_EXTENTION = "keycloakThemes";
   private UploadedFile file;
   private StreamedContent theme;
   private String referenceTheme;
-  private static final String LOGIN_FTL_PATH = "theme/%s/login/login.ftl";
-  private static final String DEFAULT_REGISTER_URL_KEY = "${url.registration}";
-  private static final String CUSTOM_THEME_LOGIN_TEMPLATE_PATH = "custom-theme/login/login.ftl";
-  private static final String AVAILABLE_THEME_PATH = "META-INF/keycloak-themes.json";
-  private static final String CUSTOM_THEME_PROPERTY_TEMPLATE_PATH = "custom-theme/login/theme.properties";
-  private static final String THEME_PROPERTIES_CONTENT = "parent=%s\nimport=common/keycloak\n";
-
   private List<String> themes;
   private String registerUrl;
+  private JarFile jarFile;
 
-  public void upload() {
-    createCustomThemeAsZip();
+  public void getModifiedTheme() {
+    theme = JarUtils.processJarFile(jarFile, referenceTheme, registerUrl);
   }
-
-
 
   public List<String> readJarFile(FileUploadEvent event) {
     this.file = event.getFile();
-    themes = new ArrayList<>();
     if (Objects.isNull(file)) {
-      return themes;
+      return new ArrayList<>();
     }
-
     try (InputStream inputStream = file.getInputStream()) {
-      Path tempFile = Files.createTempFile("keycloakThemes", ".jar");
+      Path tempFile = Files.createTempFile(TEMP_FILE_NAME, JAR_EXTENTION);
       Files.copy(inputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
-
-      try (JarFile jarFile = new JarFile(tempFile.toFile())) {
-        ZipEntry themesEntry = jarFile.getEntry(AVAILABLE_THEME_PATH);
-
-        if (themesEntry != null) {
-          try (InputStream themesInputStream = jarFile.getInputStream(themesEntry)) {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode rootNode = mapper.readTree(themesInputStream);
-            JsonNode themesNode = rootNode.path("themes");
-            if (themesNode.isArray()) {
-              for (JsonNode themeNode : themesNode) {
-                JsonNode nameNode = themeNode.path("name");
-                if (nameNode.isTextual()) {
-                  themes.add(nameNode.asText());
-                }
-              }
-            }
-          }
-        }
-      }
+      jarFile = new JarFile(tempFile.toFile());
+      themes = JarUtils.readThemeNameFromJar(jarFile);
       Files.deleteIfExists(tempFile);
     } catch (IOException e) {
       e.printStackTrace();
     }
     return themes;
-  }
-
-  public void updateThemes() {
-
-  }
-
-  public StreamedContent createCustomThemeAsZip() {
-    try {
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      try (ZipOutputStream zipOutputStream = new ZipOutputStream(baos)) {
-        addModifiedLoginFtlToZip(zipOutputStream);
-        addThemePropertiesToZip(zipOutputStream);
-      }
-      byte[] zipBytes = baos.toByteArray();
-      ByteArrayInputStream inputStream = new ByteArrayInputStream(zipBytes);
-
-      return DefaultStreamedContent.builder().contentType("application/zip").name("custom-theme.zip")
-          .stream(() -> inputStream).build();
-    } catch (IOException e) {
-      throw new RuntimeException("Error creating theme ZIP: " + e.getMessage(), e);
-    }
-  }
-
-  private String getFileContentFromJar(String filePath) {
-    if (file == null || filePath == null || filePath.isEmpty()) {
-      return null;
-    }
-
-    try {
-      Path tempFile = Files.createTempFile("uploadedJar", ".jar");
-      Files.write(tempFile, file.getContent());
-
-      try (JarFile jarFile = new JarFile(tempFile.toFile())) {
-        JarEntry entry = jarFile.getJarEntry(filePath);
-        if (entry == null) {
-          Files.deleteIfExists(tempFile);
-          return null;
-        }
-
-        try (BufferedReader reader = new BufferedReader(
-            new InputStreamReader(jarFile.getInputStream(entry), StandardCharsets.UTF_8))) {
-          StringBuilder content = new StringBuilder();
-          String line;
-          while ((line = reader.readLine()) != null) {
-            content.append(line).append("\n");
-          }
-          Files.deleteIfExists(tempFile);
-          return content.toString();
-        }
-      } finally {
-        Files.deleteIfExists(tempFile);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-      return null;
-
-    }
-  }
-
-  private void addModifiedLoginFtlToZip(ZipOutputStream zipOutputStream) throws IOException {
-    String content = getFileContentFromJar(String.format(LOGIN_FTL_PATH, referenceTheme));
-    String modifiedContent = content.replace(DEFAULT_REGISTER_URL_KEY, registerUrl);
-    ZipEntry entry = new ZipEntry(CUSTOM_THEME_LOGIN_TEMPLATE_PATH);
-    zipOutputStream.putNextEntry(entry);
-    zipOutputStream.write(modifiedContent.getBytes(StandardCharsets.UTF_8));
-    zipOutputStream.closeEntry();
-  }
-
-  private void addThemePropertiesToZip(ZipOutputStream zipOutputStream) throws IOException {
-    String propertiesContent = String.format(THEME_PROPERTIES_CONTENT, referenceTheme);
-    ZipEntry entry = new ZipEntry(CUSTOM_THEME_PROPERTY_TEMPLATE_PATH);
-    zipOutputStream.putNextEntry(entry);
-    zipOutputStream.write(propertiesContent.getBytes(StandardCharsets.UTF_8));
-    zipOutputStream.closeEntry();
   }
 
   public UploadedFile getFile() {
@@ -201,5 +88,13 @@ public class AdminManagementBean {
 
   public void setRegisterUrl(String registerUrl) {
     this.registerUrl = registerUrl;
+  }
+
+  public JarFile getJarFile() {
+    return jarFile;
+  }
+
+  public void setJarFile(JarFile jarFile) {
+    this.jarFile = jarFile;
   }
 }
